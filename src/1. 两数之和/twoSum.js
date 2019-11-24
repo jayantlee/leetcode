629f956a012acd11740311a9eff4d9af7e9cc982
jayantlee <jayantlee@163.com>
2019-11-25 01:57:02 +0800

    fix: twoSum, 'numMap[nums[i]]' to 'numMap[nums[i]] != null'

--- src/1. 两数之和/twoSum.js
+++ src/1. 两数之和/twoSum.js
@@ -2,7 +2,7 @@ export default function twoSum(nums, target) {
   const numMap = {};
   let r = [];
   for (let i = 0; i < nums.length; i++) {
-    if (numMap[nums[i]]) {
+    if (numMap[nums[i]] != null) {
       r = [numMap[nums[i]], i];
       break;
     } else {
--- src/1. 两数之和/twoSum.test.js
+++ src/1. 两数之和/twoSum.test.js
@@ -3,3 +3,7 @@ import twoSum from "./twoSum";
 test("1. 两数之和", () => {
   expect(twoSum([2, 4, 6], 10)).toEqual([1, 2]);
 });
+
+test("1. 两数之和", () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
