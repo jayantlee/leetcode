97ce846f23d542f20fea2c1c5c8fa5472192ad99
jayantlee <jayantlee@163.com>
2019-12-02 21:30:21 +0800

    add: 41.缺失的第一个正数,215.数组中的第 k 个最大元素

---
+++ src/1-50/215.数组中的第k个最大元素.test.js
@@ -0,0 +1,5 @@
+import findKthLargest from "./215.数组中的第k个最大元素";
+
+test("215.数组中的第k个最大元素", () => {
+  expect(findKthLargest([9, 3, 1, 5, 3], 2)).toBe(5);
+});
---
+++ src/1-50/41.缺失的第一个正数.test.js
@@ -0,0 +1,5 @@
+import firstMissingPositive from "./41.缺失的第一个正数";
+
+test("41.缺失的第一个正数", () => {
+  expect(firstMissingPositive([1, 2, 0])).toBe(3);
+});
